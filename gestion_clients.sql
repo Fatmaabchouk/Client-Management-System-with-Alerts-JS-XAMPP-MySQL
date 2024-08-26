9e8850ac7f5be185bae6b98091e8a0b38e10365e
Fatmaabchouk <abchoukfatma@gmail.com>
2024-08-26 22:07:38 +0100

    first commit

---
+++ gestion_clients.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 26 août 2024 à 22:41
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `gestion_clients`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `clients`
+--
+
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `alerte_envoyee` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `clients`
+--
+
+INSERT INTO `clients` (`id`, `nom`, `alerte_envoyee`) VALUES
+(7, 'fatma', 0),
+(8, 'mouhib', 0),
+(9, 'emna', 0);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const path = require('path');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '', // Remplacez par votre mot de passe MySQL
+    database: 'gestion_clients'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connecté à la base de données MySQL.');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/ajouter-client', (req, res) => {
+    const nomClient = req.body.nom;
+    connection.query('INSERT INTO clients (nom) VALUES (?)', [nomClient], (err, result) => {
+        if (err) throw err;
+        res.redirect('/liste.html');
+    });
+});
+
+app.get('/liste-clients', (req, res) => {
+    connection.query('SELECT * FROM clients', (err, results) => {
+        if (err) throw err;
+        res.send(results);
+    });
+});
+
+app.post('/supprimer-client', (req, res) => {
+    const clientId = req.body.id;
+    connection.query('DELETE FROM clients WHERE id = ?', [clientId], (err, result) => {
+        if (err) throw err;
+        res.sendStatus(200);
+    });
+});
+
+app.post('/envoyer-alerte', (req, res) => {
+    const { id, alerte_activee } = req.body;
+    const nouvelleValeur = alerte_activee == 1 ? 1 : 0; // 1 pour activer, 0 pour désactiver
+    connection.query('UPDATE clients SET alerte_envoyee = ? WHERE id = ?', [nouvelleValeur, id], (err, result) => {
+        if (err) throw err;
+        res.sendStatus(200);
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('Le serveur tourne sur le port 3000.');
+});
